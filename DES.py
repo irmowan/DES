1f0e7ccf3ef8cd7d9e28051d4fcdd6e498241b3b
wanyimu <13307130174@fudan.edu.cn>
2015-09-30 23:28:58 +0800

    Basic DES with 64-bit plaintext & 64-bit key

--- DES.py
+++ DES.py
@@ -1,6 +1,34 @@
 __author__ = 'irmo'
 import re
 
+IP = [
+    57, 49, 41, 33, 25, 17, 9, 1,
+    59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5,
+    63, 55, 47, 39, 31, 23, 15, 7,
+    56, 48, 40, 32, 24, 16, 8, 0,
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6]
+IP_inverse = [
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41, 9, 49, 17, 57, 25,
+    32, 0, 40, 8, 48, 16, 56, 24]
+E = [
+    31, 0, 1, 2, 3, 4,
+    3, 4, 5, 6, 7, 8,
+    7, 8, 9, 10, 11, 12,
+    11, 12, 13, 14, 15, 16,
+    15, 16, 17, 18, 19, 20,
+    19, 20, 21, 22, 23, 24,
+    23, 24, 25, 26, 27, 28,
+    27, 28, 29, 30, 31, 0]
+
 Sbox = [
     # S0
     [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
@@ -65,6 +93,7 @@ PC1 = [
     6, 61, 53, 45, 37, 29, 21,
     13, 5, 60, 52, 44, 36, 28,
     20, 12, 4, 27, 19, 11, 3]
+
 PC2 = [
     13, 16, 10, 23, 0, 4,
     2, 27, 14, 5, 20, 9,
@@ -74,18 +103,57 @@ PC2 = [
     29, 39, 50, 44, 32, 47,
     43, 48, 38, 55, 33, 52,
     45, 41, 49, 35, 28, 31]
+
+
+def s_box(sbox_in):
+    sbox_out = []
+    for i in range(8):
+        box_in = sbox_in[6 * i:6 * i + 6]
+        row = box_in[0] * 2 + box_in[5]
+        column = box_in[1] * 8 + box_in[2] * 4 + box_in[3] * 2 + box_in[4]
+        num = Sbox[i][row * 16 + column]
+        bin_num = '{0:04b}'.format(num)
+        box_out = [int(bin_num[x]) for x in range(4)]
+        sbox_out += box_out
+    return sbox_out
+
+
+def f(R, K):
+    R_EP = [R[x] for x in E]
+    sbox_in = list_xor(R_EP, K)
+    sbox_out = s_box(sbox_in)
+    return sbox_out
+
+
+def list_xor(x, y):
+    return [(a[0] ^ a[1]) for a in list(zip(x, y))]
+
+
 print("hello, DES!")
+
+
 print("Please input your primary key:")
 while True:
     Key = input().strip()
+    if len(Key) != 64:
+        print('The length of Key should be 64 bits, please check it and input again.')
+        continue
     if not (re.match('[01]*[^01]', Key)):
         break
-    print('The input Key is not a 01 string, please check it.')
+    print('The input Key is not a 01 string, please check it and input again.')
 print(Key)
+
 # Key = '1010101010101010101010101010101010101010101010101010101010101010'
+Message = '1010101010101010101010101010101010101010101010101010101010101010'
+print('Key:         ', Key)
+print('Plain Text:  ', Message)
+Key = [int(x) for x in Key]
+Message = [int(x) for x in Message]
 C = []
 D = []
 K = []
+L = []
+R = []
 C.append([Key[x] for x in PC1[0:28]])
 D.append([Key[x] for x in PC1[28:56]])
 for i in range(16):
@@ -94,4 +162,18 @@ for i in range(16):
     D.append(D[-1][move:] + D[-1][:move])
     CD = C[-1] + D[-1]
     K.append([CD[x] for x in PC2])
-    print(K[-1])
+L.append([Message[x] for x in IP[0:32]])
+R.append([Message[x] for x in IP[32:64]])
+for i in range(15):
+    L.append(R[i])
+    R.append(list_xor(L[i], f(R[i], K[i])))
+# 16th Round
+L.append(list_xor(L[15], f(R[15], K[15])))
+R.append(L[15])
+LR = L[16] + R[16]
+cipher = [LR[x] for x in IP_inverse]
+
+s = ""
+for i in range(64):
+    s += str(cipher[i])
+print('Cipher Text: ', s)
